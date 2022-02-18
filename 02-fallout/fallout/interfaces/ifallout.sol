01432d90db084557ed0dcbdb03942a9200d95634
Patrice Tisserand <p.tisserand@gmail.com>
2022-02-18 18:58:31 +0100

    fallout: added missing interface file

---
+++ 02-fallout/fallout/interfaces/ifallout.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+interface IFallout {
+    function owner() external view returns (address payable);
+
+    function Fal1out() external payable;
+}
